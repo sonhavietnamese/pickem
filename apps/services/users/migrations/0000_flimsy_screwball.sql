249cd2ed0b9cd7da7eaaae185ecb38b7631ab97f
sonhavietnamese <nguyensonha.hanz@gmail.com>
2026-01-22 20:24:30 +0700

    feat: add db

---
+++ apps/services/users/migrations/0000_flimsy_screwball.sql
@@ -0,0 +1,6 @@
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"surname" text NOT NULL,
+	CONSTRAINT "users_surname_unique" UNIQUE("surname")
+);
--- apps/web/env.ts
+++ apps/web/env.ts
@@ -3,8 +3,10 @@ import { z } from 'zod'
 
 export const env = createEnv({
   server: {
+    PRIVY_APP_SECRET: z.string().min(1),
     FUNDING_PRIVATE_KEY: z.string().min(1),
     SOLANA_RPC_URL: z.url(),
+    SIGNER_PRIVATE_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_PRIVY_APP_ID: z.string().min(1),
@@ -18,5 +20,7 @@ export const env = createEnv({
 
     FUNDING_PRIVATE_KEY: process.env.FUNDING_PRIVATE_KEY,
     SOLANA_RPC_URL: process.env.SOLANA_RPC_URL,
+    PRIVY_APP_SECRET: process.env.PRIVY_APP_SECRET,
+    SIGNER_PRIVATE_KEY: process.env.SIGNER_PRIVATE_KEY,
   },
 })
